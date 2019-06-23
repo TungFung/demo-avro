c112ccc5380cbeb5a6797422601094e7cdbbde0f
Eric <516629105@qq.com>
2019-06-23 09:30:18 +0800

    first commit

---
+++ src/main/java/com/example/serialize/SerializeMain.java
@@ -0,0 +1,56 @@
+package com.example.serialize;
+
+import com.example.avro.User;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+
+/**
+ * user.avsc是schema
+ * users.avro是数据文件
+ */
+public class SerializeMain {
+
+    public static void main(String[] args) throws Exception {
+        User user1 = new User();
+        user1.setName("Alyssa");
+        user1.setFavoriteNumber(256);
+        // Leave favorite color null
+
+        // Alternate constructor
+        User user2 = new User("Ben", 7, "red");
+
+        // Construct via builder
+        User user3 = User.newBuilder()
+                .setName("Charlie")
+                .setFavoriteColor("blue")
+                .setFavoriteNumber(null)
+                .build();
+
+        // Serialize user1, user2 and user3 to disk
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<>(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<>(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), new File("users.avro"));//写入到哪个文件
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.append(user3);
+        dataFileWriter.close();
+
+        // Deserialize Users from disk
+        DatumReader<User> userDatumReader = new SpecificDatumReader<User>(User.class);
+        DataFileReader<User> dataFileReader = new DataFileReader<User>(new File("users.avro"), userDatumReader);//从哪个文件读取
+        User user = null;
+        while (dataFileReader.hasNext()) {
+            // Reuse user object by passing it to next(). This saves us from
+            // allocating and garbage collecting many objects for files with
+            // many items.
+            user = dataFileReader.next(user);//每次读一行对象记录
+            System.out.println(user);
+        }
+    }
+}
